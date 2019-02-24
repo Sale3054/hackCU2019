55c45dfc5521af506698e35e6850d301bbf2723d
p-lucero <p-lucero@users.noreply.github.com>
2019-02-23 19:10:25 -0700

    Stub some functions

--- background.js
+++ background.js
@@ -1,1 +1,29 @@
-console.log("Hello World!")
+console.log("Creating the WebHistoryGraph extension!")
+
+var graph_headnodes = {}
+var graph_roots = []
+
+function handle_tab_creation(tab)
+{
+
+}
+
+function handle_tab_update(tabID, changeInfo, tab)
+{
+
+}
+
+function handle_tab_removal(tabID, removeInfo)
+{
+	
+}
+
+function handle_target_creation(details)
+{
+	
+}
+
+browser.tabs.onCreated.addListener(handle_tab_creation)
+browser.tabs.onUpdated.addListener(handle_tab_update)
+browser.tabs.onRemoved.addListener(handle_tab_removal)
+browser.webNavigation.onCreatedNavigationTarget.addListener(handle_target_creation)
